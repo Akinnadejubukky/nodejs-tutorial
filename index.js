3cbd5ea6c7625e28bfa93d91cf68f0e071d23fe8
akinnadeju bukky <bukky.akinnadeju17@gmail.com>
2020-04-26 22:37:22 +0100

    added server

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+
+
+// const cors = require("cors")
+// const express = require("express");
+
+
+
+const server = http.createServer((req, res) => {
+    res.write("HelloWorld");
+    res.end()
+});
+
+
+server.listen(5000,(err)=>{
+    console.log("Listening on port 5000")
+});
